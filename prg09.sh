c44e22a04255dc93663475427c1f5b5b0e5a03bd
CSprograms <78929938+CSprograms@users.noreply.github.com>
2023-05-16 08:42:37 +0530

    Update prg09.sh

--- prg09.sh
+++ prg09.sh
@@ -1,20 +1,14 @@
+#Summing up 1^2 + 2^2 + 3^2 + 4^2 + ... + n^2
 #!/bin/bash
 
-# Get the value of n from user input
 read -p "Enter the value of n: " n
 
-# Initialize a variable to hold the sum
 sum=0
 
-# Loop through numbers from 1 to n and add the square of each one to the sum
 for ((i=1; i<=n; i++))
 do
-    # Compute the square of the current number (i.e., i*i)
     square=$((i*i))
-
-    # Add the current square to the sum
     sum=$((sum + square))
 done
 
-# Print the sum to the console
 echo "The sum of the series is: $sum"
