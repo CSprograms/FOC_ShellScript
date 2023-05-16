fcf43d6c0741a3cd7fcb523d87f1f94699c7a157
CSprograms <78929938+CSprograms@users.noreply.github.com>
2023-05-16 07:58:35 +0530

    Update prg01.sh

--- prg01.sh
+++ prg01.sh
@@ -1,9 +1,8 @@
+#Generation of number series 1, 2, 3, 4,…..n
 #!/bin/bash
 
-# Get the value of n from user input
 read -p "Enter the value of n: " n
 
-# Loop through numbers from 1 to n and print each one
 for ((i=1; i<=n; i++))
 do
     echo $i
