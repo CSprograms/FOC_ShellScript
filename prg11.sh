7599292911c4419813acec111b484bd2c1ab77dd
CSprograms <78929938+CSprograms@users.noreply.github.com>
2023-05-16 07:59:29 +0530

    Create prg11.sh

---
+++ prg11.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Check if argument is provided
+if [ $# -eq 0 ]; then
+  echo "Please provide a number n as argument."
+  exit 1
+fi
+
+n=$1
+sum=0
+
+for (( i=1; i<=n; i++ ))
+do
+  power=$((i**i))
+  sum=$((sum + power))
+done
+
+echo "The sum of 1^1 + 2^2 + 3^3 + ... + $n^$n is: $sum"
