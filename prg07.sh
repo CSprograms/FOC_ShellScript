8e503078ff83ea77c576c985a1cd9c3f5b76d158
CSprograms <78929938+CSprograms@users.noreply.github.com>
2023-05-16 07:59:14 +0530

    Update prg07.sh

--- prg07.sh
+++ prg07.sh
@@ -1,16 +1,12 @@
+#Summing up Odd Number series
 #!/bin/bash
 
-# Get the value of n from user input
 read -p "Enter the value of n: " n
-
-# Initialize a variable to hold the sum
 sum=0
 
-# Loop through odd numbers from 1 to n and add each one to the sum
 for ((i=1; i<=n; i+=2))
 do
     sum=$((sum + i))
 done
 
-# Print the sum to the console
 echo "The sum of the odd number series is: $sum"
