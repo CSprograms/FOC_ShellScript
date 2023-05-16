d949c1cafb349668e0322f60c6043ee2c92f7af4
CSprograms <78929938+CSprograms@users.noreply.github.com>
2023-05-16 07:58:48 +0530

    Update prg03.sh

--- prg03.sh
+++ prg03.sh
@@ -1,9 +1,8 @@
+#Generation of ODD number series 1, 3, 5, …..n
 #!/bin/bash
 
-# Get the value of n from user input
 read -p "Enter the value of n: " n
 
-# Loop through odd numbers from 1 to n and print each one
 for ((i=1; i<=n; i+=2))
 do
     echo $i
