2692f944d0bdc1e116aad3e398e2c17bd86d57a6
CSprograms <78929938+CSprograms@users.noreply.github.com>
2023-05-16 07:58:41 +0530

    Update prg02.sh

--- prg02.sh
+++ prg02.sh
@@ -1,9 +1,8 @@
+#Generation of even number series 2, 4, 6, …..n
 #!/bin/bash
 
-# Get the value of n from user input
 read -p "Enter the value of n: " n
 
-# Loop through even numbers from 2 to n and print each one
 for ((i=2; i<=n; i+=2))
 do
     echo $i
