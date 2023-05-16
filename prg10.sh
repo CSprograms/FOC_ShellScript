1a2bc901dbe966972d167f01cb570b6b4ced49b6
CSprograms <78929938+CSprograms@users.noreply.github.com>
2023-05-16 08:42:42 +0530

    Update prg10.sh

--- prg10.sh
+++ prg10.sh
@@ -1,20 +1,14 @@
+#Summing up 2^2 + 4^2 + 6^2 + 8^2 + ... + n^2
 #!/bin/bash
 
-# Get the value of n from user input
 read -p "Enter the value of n: " n
 
-# Initialize a variable to hold the sum
 sum=0
 
-# Loop through even numbers from 2 to n and add the square of each one to the sum
 for ((i=2; i<=n; i+=2))
 do
-    # Compute the square of the current even number (i.e., i*i)
     square=$((i*i))
-
-    # Add the current square to the sum
     sum=$((sum + square))
 done
 
-# Print the sum to the console
 echo "The sum of the series is: $sum"
