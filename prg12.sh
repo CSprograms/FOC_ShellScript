4b330b5f986643ef467a7ad468a3a6253fa788ef
CSprograms <78929938+CSprograms@users.noreply.github.com>
2023-05-16 07:59:36 +0530

    Create prg12.sh

---
+++ prg12.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Check if argument is provided
+if [ $# -eq 0 ]; then
+  echo "Please provide a number n as argument."
+  exit 1
+fi
+
+n=$1
+sum=0
+
+for (( i=1; i<=n; i+=2 ))
+do
+  square=$((i*i))
+  sum=$((sum + square))
+done
+
+echo "The sum of squares of odd numbers up to $n is: $sum"
