7091733ef09120c7c02a5dc64a4aae7410e93736
CSprograms <78929938+CSprograms@users.noreply.github.com>
2023-05-16 07:59:02 +0530

    Update prg05.sh

--- prg05.sh
+++ prg05.sh
@@ -1,16 +1,12 @@
+#Summing up series 1 + 2 + 3 + 4….. +n
 #!/bin/bash
 
-# Get the value of n from user input
 read -p "Enter the value of n: " n
-
-# Initialize a variable to hold the sum
 sum=0
 
-# Loop through numbers from 1 to n and add each one to the sum
 for ((i=1; i<=n; i++))
 do
     sum=$((sum + i))
 done
 
-# Print the sum to the console
 echo "The sum of the series is: $sum"
