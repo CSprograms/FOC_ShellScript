e0cb9516ddeab37ede080e8362beca58d8c06f7a
CSprograms <78929938+CSprograms@users.noreply.github.com>
2023-05-23 08:09:34 +0530

    Create prg19.sh

---
+++ prg19.sh
@@ -0,0 +1,23 @@
+#Revering the digits of an integer number
+#!/bin/bash
+
+reverse_digits() {
+  local number=$1
+  local reversed=""
+
+  while [ $number -gt 0 ]
+  do
+    local digit=$((number % 10))
+    reversed="${reversed}${digit}"
+    number=$((number / 10))
+  done
+
+  echo "$reversed"
+}
+
+read -p "Enter an integer number: " input_number
+
+reversed_number=$(reverse_digits "$input_number")
+
+# Output the reversed number
+echo "Reversed number: $reversed_number"
